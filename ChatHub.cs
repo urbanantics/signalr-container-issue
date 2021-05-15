93e920639b837221947e0a7cc6175214f840a912
Reuben <reubenhutton@hotmail.com>
2021-05-15 12:13:25 +0100

    example signalr client

---
+++ ChatHub.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace delete_me
+{
+    public class ChatHub : Hub
+    {
+        public Task BroadcastMessage(string name, string message) =>
+            Clients.All.SendAsync("broadcastMessage", name, message);
+
+        public Task Echo(string name, string message) =>
+            Clients.Client(Context.ConnectionId)
+                   .SendAsync("echo", name, $"{message} (echo from server)");
+    }
+}
